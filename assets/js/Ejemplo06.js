65cb50fc55762260cd8954d73088d1304cc8dce2
kodigo-denny <denny.herrera@kodigo.org>
2022-12-02 22:19:02 -0600

    Cambios 02/02/2022

---
+++ assets/js/Ejemplo06.js
@@ -0,0 +1,23 @@
+let array = [1, 12, 2.5, null, 'John #', true, 100, 50]
+
+/*
+console.log(array[3])
+
+array[3] = 150
+
+console.log(array[4][1])
+*/
+
+array[8] = 200
+
+for(let i = 0; i < array.length; i++){
+    console.log("Posición: " + i + " Valor: " + array[i] + " Tipo de dato: " + typeof(array[i]))
+}
+
+
+let vector = []
+
+/*
+console.log(array)
+console.log(array.length)
+*/
